a4ceb343f4f9f8f5f66db566ea98cd5ca5984ab5
Ville Laitinen <ville.o.laitinen@edu.turkuamk.fi>
2023-05-02 04:39:34 +0300

    11.9 complete

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -4,4 +4,11 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('individual pokemon page can be accessed', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('overgrow')
+    cy.contains('chlorophyll')
+  })
 })
