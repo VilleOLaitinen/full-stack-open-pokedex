4767dde31fd8096287d84c21f67174411a6b6b7f
Ville Laitinen <ville.o.laitinen@edu.turkuamk.fi>
2023-05-08 04:35:14 +0300

    11.12 complete

--- app.js
+++ app.js
@@ -6,11 +6,9 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 app.get('/version', (req, res) => {
-  res.send('4')
+  res.send('5')
 })
 app.get('/health', (req, res) => {
-  throw 'error...'
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
